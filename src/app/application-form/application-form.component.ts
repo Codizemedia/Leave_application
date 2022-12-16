cfc83ee37ac7ae58a2b6605e47bff42896dfd3a0
MarVills <villaflormarbenc@gmail.com>
2022-12-17 06:24:59 +0800

    using ng swtch to shorten codes

--- src/app/application-form/application-form.component.ts
+++ src/app/application-form/application-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FormControl, Validators } from '@angular/forms';
+import { choicesA, choicesB } from './form-questions';
+ 
 
 @Component({
   selector: 'app-application-form',
@@ -11,6 +13,8 @@ export class ApplicationFormComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup = Object.create(null);
   secondFormGroup: FormGroup = Object.create(null);
+  choices1 = choicesA
+  choices2 = choicesB
 
   constructor(private _formBuilder: FormBuilder) { }
 
