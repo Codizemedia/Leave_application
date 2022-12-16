848f14a89e3eaf7bcecf2a604aab9f0cd012039d
MarVills <villaflormarbenc@gmail.com>
2022-12-16 23:03:47 +0800

    ready for optimization

--- src/app/application-form/application-form.component.ts
+++ src/app/application-form/application-form.component.ts
@@ -27,21 +27,4 @@ export class ApplicationFormComponent implements OnInit {
     console.log("event", event)
   }
 
-  public choices: Array<any> = [
-    {description: 'Vacation Leave', value: false},
-    {description: "Mandatory/Force Leave", value: false},
-    {description: "Sick Leave", value: false},
-    {description: "Maternity Leave", value: false},
-    {description: "Paternity Leave", value: false},
-    {description: "Special Privilege Leave", value: false},
-    {description: "Solo Parent Leave", value: false},
-    {description: "Study Leave", value: false},
-    {description: "10-Day VAWC Leave", value: false},
-    {description: "Rehabilitation Privilege", value: false},
-    {description: "Special Leave Benefits for Women", value: false},
-    {description: "Special Emergency", value: false},
-    {description: "Adoption Leave", value: false},
-  ];
-
-
 }
---
+++ src/app/application-form/form-questions.ts
@@ -0,0 +1,197 @@
+
+export const choicesA: Array<any> = [
+  {
+    type: 'checkbox', 
+    description: 'Vacation Leave', 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Mandatory/Force Leave", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Sick Leave", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Maternity Leave", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Paternity Leave", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Special Privilege Leave", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Solo Parent Leave", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Study Leave", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "10-Day VAWC Leave", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Rehabilitation Privilege", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Special Leave Benefits for Women", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Special Emergency", 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: "Adoption Leave", 
+    value: false
+  },
+  {
+    type: 'space', 
+    description: '', 
+    value: null
+  },
+  {
+    type: 'label', 
+    description: 'Others:', 
+    value: null
+  },
+  {
+    type: 'underline-label', 
+    description: 'TLB (Termianl Leave Benefits)', 
+    value: null
+  },
+  {
+    type: 'label', 
+    description: 'C. NUMBER OF WORKING DAYS APPLIED FOR', 
+    value: null
+  },
+  {
+    type: 'input', 
+    description: '', 
+    value: null
+  },
+  {
+    type: 'label', 
+    description: 'INCLUSIVE DATES', 
+    value: null
+  },
+  {
+    type: 'input', 
+    description: '', 
+    value: null
+  },
+];
+
+export const choicesB: Array<any> = [
+  {
+    type: 'italic-label', 
+    description: 'In case of VAcation/Special Privelege Leave:', 
+    value: null
+  },
+  {
+    type: 'checkbox-input', 
+    description: 'Within the Philippines', 
+    value: false
+  },
+  {
+    type: 'checkbox-input', 
+    description: 'Abroad (Specify)', 
+    value: false
+  },
+  {
+    type: 'italic-label', 
+    description: 'In case of Sick Leave:', 
+    value: null
+  },
+  {
+    type: 'checkbox-input', 
+    description: 'In Hospital (Specify Illness)', 
+    value: false
+  },
+  {
+    type: 'checkbox-input', 
+    description: 'Out Patient (Specify Illness)', 
+    value: false
+  },
+  {
+    type: 'italic-label', 
+    description: 'In case of Special Leave Benefits for Women:', 
+    value: null
+  },
+  {
+    type: 'italic-label-input', 
+    description: '(Specify Illnes)', 
+    value: null
+  },
+  {
+    type: 'italic-label', 
+    description: 'Incase of Study Leave:', 
+    value: null
+  },
+  {
+    type: 'checkbox-input', 
+    description: "Completion of Master's Degree", 
+    value: false
+  },
+  {
+    type: 'checkbox-input', 
+    description: 'Bar/Board Examination Review', 
+    value: false
+  },
+  {
+    type: 'italic-label', 
+    description: 'Other Purpose:', 
+    value: null
+  },
+  {
+    type: 'checkbox-input', 
+    description: 'Monetization of Leave Credits', 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: 'Terminal Leave', 
+    value: false
+  },
+  {
+    type: 'space', 
+    description: '', 
+    value: null
+  },
+  {
+    type: 'label', 
+    description: 'D. COMMUTATION', 
+    value: null
+  },
+  {
+    type: 'checkbox', 
+    description: 'Not Requested', 
+    value: false
+  },
+  {
+    type: 'checkbox', 
+    description: 'Requested', 
+    value: false,
+  }
+  
+];
