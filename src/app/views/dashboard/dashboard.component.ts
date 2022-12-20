79662ef1ced6a6bc7ec3c99e5e1a65ff3df515f0
MarVills <villaflormarbenc@gmail.com>
2022-12-20 21:58:34 +0800

    print as pdf with headers

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -74,7 +74,8 @@ export class DashboardComponent implements OnInit {
      html2canvas(DATA).then((canvas) => {
       console.log("see canvaas", canvas)
        let fileWidth = 208;
-       let fileHeight = (canvas.height * fileWidth) / canvas.width;
+       let fileHeight = ((canvas.height * fileWidth) / canvas.width) -50 ;
+       console.log("file height", fileHeight)
        const FILEURI = canvas.toDataURL('image/png');
        let PDF = new jsPDF('p', 'mm', 'a4');
        let position = 0;
